475b181012c6ba7e1896884cf7ec09db82686ea8
Denisa66 <157240627+Denisa66@users.noreply.github.com>
2025-03-27 18:09:30 +0200

    Implementare problema (Animal sounds)
    Creare proiect de tip consola

---
+++ Animal_Sounds/Animal_Sounds/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Cat : Animal
+{
+    public override string Name => "Cat";
+    public override string MakeSound() => "Meow!";
+}
+
---
+++ Animal_Sounds/Animal_Sounds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        List<Animal> animals = new List<Animal>
+        {
+            new Dog(),
+            new Cat(),
+            new Wolf() 
+        };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.Name} makes sound: {animal.MakeSound()}");
+        }
+
+        Console.ReadLine();
+    }
+}
